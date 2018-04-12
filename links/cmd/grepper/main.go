2fedabacaa974a3afb48df7372e629a0be261d26
Torben <torbinsky+ghtorbensky@gmail.com>
2018-04-11 21:02:44 -0700

    TW: Main cmd now very simply demonstrates the link find at work.

--- links/cmd/grepper/main.go
+++ links/cmd/grepper/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/torbensky/gophercises/links"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	for _, f := range []string{"ex1.html", "ex2.html", "ex3.html", "ex4.html"} {
+		fmt.Printf("links in %s:\n", f)
+		printLinks(f)
+	}
+}
+
+func printLinks(filename string) {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ls, err := links.Find(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, l := range ls {
+		fmt.Println(l)
+	}
 }
